bebdb507f696d8c5ee57a4e36053342bf82a274a
keshav <bkesha.anaar@gmail.com>
2017-12-19 10:07:46 +0545

    basic app setup with loginform

---
+++ esambriddhi/login/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+#this will help identify to which app the url is associated
+app_name = 'login'
+
+urlpatterns = [
+    url(r'^$', views.homeView, name='HomeView'),
+]
